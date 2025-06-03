1b1b919d55e944a761344277955b56d4ef543883
Brady <bradycai26@gmail.com>
2025-06-03 12:14:59 -0400

    Merge remote-tracking branch 'origin/main'

--- allofit/src/calendar/Event.java
+++ allofit/src/calendar/Event.java
@@ -1,4 +1,37 @@
 package calendar;
 
+import java.time.LocalDateTime;
+
 public class Event {
+  private String subject;
+  private LocalDateTime start;
+  private LocalDateTime end;
+
+  public Event(String subject, LocalDateTime start, LocalDateTime end) {
+    if (subject == null || subject.isEmpty()) {
+      throw new IllegalArgumentException("Subject cannot be null or empty");
+    }
+    if (start == null || start.isAfter(end)) {
+      throw new IllegalArgumentException("Must need a valid start date");
+    }
+    if (end != null || end.isBefore(start)) {
+      throw new IllegalArgumentException("End date cannot be before start date");
+    }
+
+    this.subject = subject;
+    this.start = start;
+    this.end = end;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public LocalDateTime getStart() {
+    return start;
+  }
+
+  public LocalDateTime getEnd() {
+    return end;
+  }
 }
