dd235b14e1f8829218b869a57d1e03550571913e
Brady <bradycai26@gmail.com>
2025-06-03 13:48:49 -0400

    Merge remote-tracking branch 'origin/main'

--- allofit/src/calendar/EventSeries.java
+++ allofit/src/calendar/EventSeries.java
@@ -1,9 +1,18 @@
 package calendar;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class EventSeries {
+  private final String subject;
+  private final LocalDateTime start;
+  private final LocalDateTime end;
   private final List<Event> series;
 
-
+  public EventSeries(String subject, LocalDateTime start, LocalDateTime end, List<Event> series) {
+    this.subject = subject;
+    this.start = start;
+    this.end = end;
+    this.series = series;
+  }
 }
