b8f3f75198146ac814db91f0774753b4fb6b33f4
Brady <bradycai26@gmail.com>
2025-06-04 14:29:52 -0400

    Merge remote-tracking branch 'origin/main'

--- allofit/src/calendar/CalendarApp.java
+++ allofit/src/calendar/CalendarApp.java
@@ -1,4 +1,21 @@
 package calendar;
 
+import java.util.Scanner;
+
+/**
+ * Represents a controller.
+ */
 public class CalendarApp {
+  private final CalendarModel model;
+  private final CalendarView view;
+
+  /**
+   * Constructs the calendar app with model and view.
+   */
+  public CalendarApp() {
+    model = new CalendarModel();
+    view = new CalendarView();
+  }
+
+
 }
