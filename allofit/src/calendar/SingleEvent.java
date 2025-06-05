a07749212aaa7e86edbbd6018b6ea26cdc41d315
Brady <bradycai26@gmail.com>
2025-06-05 19:52:31 -0400

    Merge remote-tracking branch 'origin/main'

--- allofit/src/calendar/SingleEvent.java
+++ allofit/src/calendar/SingleEvent.java
@@ -6,10 +6,20 @@ package calendar;
 public class SingleEvent {
   private Event event;
 
+  /**
+   * Constructs a SingleEvent with the given event.
+   *
+   * @param event the Event to wrap
+   */
   public SingleEvent(Event event) {
     this.event = event;
   }
 
+  /**
+   * Returns the wrapped Event object.
+   *
+   * @return the Event instance
+   */
   public Event getEvent() {
     return event;
   }
